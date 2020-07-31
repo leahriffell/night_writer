2f70f5c0b6a9c0771aa0fe7653ed295881ba6ce9
Leah Riffell <leah.riffell@gmail.com>
2020-07-31 12:44:39 -0600

    Stub out ARGV in NightWriter test and create test for terminal message

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,6 +1,15 @@
 class NightWriter 
+  attr_reader :output_file
 
-files = ARGV
-puts "Created '#{files[1]}' containing 256 characters"
+  def initialize
+    @input_file = ARGV[0]
+    @output_file = ARGV[1]
+  end
 
+  def terminal_message
+    "Created '#{@output_file}' containing 256 characters"
+  end
 end
+
+test = NightWriter.new
+p test.terminal_message
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,13 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/night_writer'
 
 class NightWriterTest < MiniTest::Test 
   def setup 
+    ARGV.replace ['message.txt', 'braille.txt']
     @night_writer = NightWriter.new
   end
-
-  def test_it_exists 
+  
+  def test_it_exists
     assert_instance_of NightWriter, @night_writer
+    assert_equal 'braille.txt', @night_writer.output_file
+  end
+  
+  def test_it_can_print_message_to_terminal
+    assert_equal "Created 'braille.txt' containing 256 characters", @night_writer.terminal_message
   end
 end

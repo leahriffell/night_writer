d6ce1e8dbd60f8143bc6db1290002f5fa20b6cd3
Leah Riffell <leah.riffell@gmail.com>
2020-08-01 14:20:48 -0600

    Make text readable for Row class

--- test/row_test.rb
+++ test/row_test.rb
@@ -2,8 +2,15 @@ require './test/helper_test'
 require './lib/row'
 
 class RowTest < MiniTest::Test
-  def test_it_exists 
+  def setup 
     @row = Row.new("row row row your boat")
+  end
+
+  def test_it_exists 
     assert_instance_of Row, @row
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "row row row your boat", @row.text
   end
 end

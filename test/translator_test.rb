ecd8120d75d6cc04fd7bd5210eabf35019b6d547
Leah Riffell <leah.riffell@gmail.com>
2020-07-31 22:18:26 -0600

    Add intial test for Translator class

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,2 +1,12 @@
 require './test/helper_test'
 require './lib/translator'
+
+class TranslatorTest < MiniTest::Test 
+  def setup 
+    @translator = Translator.new
+  end
+
+  def test_it_exists
+    assert_instance_of Translator, @translator
+  end
+end

dc791e3ec3565fdbe35d8bcacc304113954714f1
Leah Riffell <leah.riffell@gmail.com>
2020-08-02 07:38:38 -0600

    Add character to braille mapping to Dictionary class

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -9,4 +9,9 @@ class DictionaryTest < MiniTest::Test
   def test_it_exists 
     assert_instance_of Dictionary, @dictionary
   end
+
+  def test_it_can_lookup_braille_for_character
+    assert_equal "0......", @dictionary.char_map[:a]
+    assert_equal "0.00..", @dictionary.char_map[:h]
+  end
 end

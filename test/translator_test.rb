1f24588a116e1a81bdd8259e3e65a67cf0eae1e9
Leah Riffell <leah.riffell@gmail.com>
2020-08-02 07:44:39 -0600

    Move dictionary from Translator as well as intial helper methods that are no longer used

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -11,17 +11,8 @@ class TranslatorTest < MiniTest::Test
   end
 
   def test_it_can_lookup_braille_for_character
-    assert_equal "0......", @translator.char_map[:a]
-    assert_equal "0.00..", @translator.char_map[:h]
-  end
-
-  def test_it_can_convert_to_multi_line
-    assert_equal "0.\n0.\n00", @translator.convert_to_multi_line("0.0.00")
-  end
-
-  def test_it_can_translate_with_braille_formatting
-    assert_equal "00\n.0\n0.", @translator.char_to_braille_with_formatting("n")
-    assert_equal "00\n.0\n00", @translator.char_to_braille_with_formatting("y")
+    assert_equal "0......", @translator.char_to_braille("a")
+    assert_equal "0.00..", @translator.char_to_braille("h")
   end
 
   def test_it_can_return_collection_of_braille_arrays
